4f22f94f45dc6f1b447207566f3ae1d58931fe64
jitendra purohit <j.purohit198@gmail.com>
2023-03-27 15:44:56 +0530

    added GA

---
+++ precache-manifest.a5c23aba20551be88c2899a384d091c3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca8db4e8f4c73b31c0f5",
+    "url": "/static/css/main.3fdd588a.chunk.css"
+  },
+  {
+    "revision": "ca8db4e8f4c73b31c0f5",
+    "url": "/static/js/main.987136f4.chunk.js"
+  },
+  {
+    "revision": "f30c58b92f877d84a012",
+    "url": "/static/js/runtime~main.137de59f.js"
+  },
+  {
+    "revision": "b851fd6f51df9fb38b80",
+    "url": "/static/js/2.c5d34f27.chunk.js"
+  },
+  {
+    "revision": "6a36256f8aac613dc048",
+    "url": "/static/js/3.cce98eaa.chunk.js"
+  },
+  {
+    "revision": "e607cc67979c3b57fbbb1c6740766b17",
+    "url": "/static/media/chargebee.e607cc67.webp"
+  },
+  {
+    "revision": "29271246230bbb6de32afa070677dee9",
+    "url": "/static/media/dart.29271246.webp"
+  },
+  {
+    "revision": "bbba944994cdaa663f6ec3e25b50f44c",
+    "url": "/static/media/flutter.bbba9449.webp"
+  },
+  {
+    "revision": "49bb08f7ab62023a6e40b5b5c660b422",
+    "url": "/static/media/firebase.49bb08f7.webp"
+  },
+  {
+    "revision": "0f7cf7faa56aa040f323af0332b95dbf",
+    "url": "/static/media/riverpod.0f7cf7fa.webp"
+  },
+  {
+    "revision": "c611f9d2fc86fa136736c082ff242fab",
+    "url": "/static/media/working.c611f9d2.png"
+  },
+  {
+    "revision": "9fdf846eedf4038fed4c915b9d66b7d7",
+    "url": "/static/media/map.9fdf846e.svg"
+  },
+  {
+    "revision": "3f5456a20bedbfd304f1bcdf30a327fd",
+    "url": "/static/media/mail.3f5456a2.svg"
+  },
+  {
+    "revision": "1cd6288296177363405902c0c4fa96c2",
+    "url": "/index.html"
+  }
+];
